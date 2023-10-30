d4b4ae85019c770a0d6b8bcf6d7135cd92c171a6
Avinash Kumar <avinash.kumar@embitel.com>
2023-10-30 15:13:30 +0530

    change in next config

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -1,31 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
-  images: {
-    domains: ["cdn3d.iconscout.com", "cdnlogo.com", "cdni.iconscout.com"],
-  },
   // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "cdn3d.iconscout.com",
-  //       port: "",
-  //       pathname: "/3d/**",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "cdnlogo.com",
-  //       port: "",
-  //       pathname: "/3d/free/**",
-  //     },
-  //     {
-  //       protocol: "https",
-  //       hostname: "cdni.iconscout.com",
-  //       port: "",
-  //       pathname: "/**",
-  //     },
-  //   ],
+  //   domains: ["cdn3d.iconscout.com", "cdnlogo.com", "cdni.iconscout.com"],
   // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
